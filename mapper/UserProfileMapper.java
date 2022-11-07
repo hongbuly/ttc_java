3047cecce4fc3747953b091b51fc1f6a46fc8caa
hongbuly <60245930+hongbuly@users.noreply.github.com>
2022-11-07 10:27:50 +0900

    Change Table Name user->userProfile
    user 테이블 이름을 userProfile로 변경.

--- mapper/UserProfileMapper.java
+++ mapper/UserProfileMapper.java
@@ -15,20 +15,20 @@ import org.apache.ibatis.annotations.Update;
 public interface UserProfileMapper {
 	
 	//getUserProfile를 호출했을 때 이 SQL문이 실행되어 UserProfile형태로 데이터를 가져옴
-	@Select("SELECT * from user WHERE email=#{email}") //파라미터와 id를 연결할 때는 $가 아니라 #로 표시해야 함.
+	@Select("SELECT * from userProfile WHERE email=#{email}") //파라미터와 id를 연결할 때는 $가 아니라 #로 표시해야 함.
 	UserProfile getUserProfile(@Param("email") String email);
 	
-	@Select("SELECT * from user")
+	@Select("SELECT * from userProfile")
 	List<UserProfile> getUserProfileList();
 	
 	//insert, update, delete 는 결과로 int값 즉 이 SQL문으로 영향을 받은 레코드 개수가 반환됨.
-	@Insert("INSERT INTO user VALUES(#{id}, #{name}, #{email}, #{pw})")
+	@Insert("INSERT INTO userProfile VALUES(#{id}, #{name}, #{email}, #{pw})")
 	int insertUserProfile(@Param("id") int id, @Param("name") String name, @Param("email") String email, @Param("pw") String pw);
 	
-	@Update("UPDATE user SET name=#{name}, pw=#{pw} WHERE email=#{email}")
+	@Update("UPDATE userProfile SET name=#{name}, pw=#{pw} WHERE email=#{email}")
 	int updateUserProfile(@Param("email") String email, @Param("pw") String pw, @Param("name") String name);
 	
 	// 이메일과 비번이 맞다면 행 개수 1개가 리턴될 것임.
-	@Select("SELECT COUNT(*) from user WHERE email=#{email} AND pw=#{pw}")
+	@Select("SELECT COUNT(*) from userProfile WHERE email=#{email} AND pw=#{pw}")
 	int getUserLogin(@Param("email") String email, @Param("pw") String pw);
 }
