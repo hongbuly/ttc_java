ce2fe0ea6e23c8c3e9a5982a332eb27d3cded4cd
hongbuly <hongbuly@naver.com>
2022-11-10 16:16:19 +0900

    Add UserFavoriteChallenge By Sunmi
    유저 찜 및 주석 추가

---
+++ mapper/UserFavoriteChallengeMapper.java
@@ -0,0 +1,23 @@
+package project.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import project.model.UserFavoriteChallenge;
+
+@Mapper
+public interface UserFavoriteChallengeMapper {
+	@Select("SELECT idchallenge FROM userFavoritechallenge WHERE idUser=#{idUser}")
+	List<UserFavoriteChallenge> getUserFavoriteChallenge(@Param("idUser") int idUser);
+	
+	@Insert("INSERT INTO userfavoritechallenge VALUES(#{idUser}, #{idChallenge})")
+	int insertUserFavoriteChallenge(@Param("idUser") int idUser, @Param("idChallenge") int idChallenge);
+	
+	@Delete("DELETE FROM userfavoritechallenge WHERE idUser=#{idUser} AND idChallenge=#{idChallenge}")
+	int deleteUserFavoriteChallenge(@Param("idUser") int idUser, @Param("idChallenge") int idChallenge);
+}
--- mapper/tagMapper.java
+++ mapper/tagMapper.java
@@ -13,9 +13,9 @@ import project.model.Tag;
 
 @Mapper
 public interface TagMapper {
-	@Select("SELECT tagName FROM tag WHERE idTag=#{idTag}")
+	@Select("SELECT tagName FROM Tag WHERE idTag=#{idTag}")
 	Tag getTag(@Param("idTag") int idTag);
 	
-	@Select("SELECT tagName FROM tag")
+	@Select("SELECT * FROM Tag")
 	List<Tag> getTagList();
 }
