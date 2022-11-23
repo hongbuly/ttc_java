72b0ce4ab5406a95491f20c4719d2243cf105564
SUNMI <84156897+SUNMI-KIM@users.noreply.github.com>
2022-11-23 21:22:21 +0900

    Update ChallengeMapper.java

--- mapper/ChallengeMapper.java
+++ mapper/ChallengeMapper.java
@@ -40,10 +40,6 @@ public interface ChallengeMapper {
 	@Select("SELECT * FROM userFavoriteChallenge uc JOIN challenge c ON uc.idChallenge = c.idChallenge JOIN userProfile u ON c.madeIdUser = u.id JOIN challengeTag ct ON c.idChallenge = ct.idChallenge JOIN tag t ON ct.idTag = t.idTag WHERE idUser=#{idUser}")
 	List<ChallengeFavoriteList> getChallengeListByIdUser(@Param("idUser") int idUser);
 	
-	//모든 챌린지 데이터 리스트 불러오기
-	@Select("SELECT * FROM challenge c JOIN userProfile uf ON c.madeIdUser = uf.id JOIN challengeTag ct ON c.idChallenge = ct.idChallenge JOIN tag t ON ct.idTag = t.idTag ORDER BY c.idChallenge")
-	List<ChallengeList> getAllChallengeList();
-
 	// 챌린지 삭제
 	@Delete("DELETE FROM challenge WHERE idChallenge=#{idChallenge}")
 	int deleteChallengeById(@Param("idChallenge") int idChallenge);
