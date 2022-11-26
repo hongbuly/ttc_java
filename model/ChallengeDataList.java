40f4ed00cb13b94863a8f1cf9ca04a2fcee9b2e4
SUNMI <84156897+SUNMI-KIM@users.noreply.github.com>
2022-11-26 12:10:26 +0900

    Update ChallengeDataList.java

--- model/ChallengeDataList.java
+++ model/ChallengeDataList.java
@@ -5,17 +5,12 @@ public class ChallengeDataList {
 	private String imageLink;
 	private int countUser;
 	private String name;
-	private String tagName1;
-	private String tagName2;
-	public ChallengeDataList(String nameChallenge, String imageLink, int countUser, String name,
-			String tagName1, String tagName2) {
+	public ChallengeDataList(String nameChallenge, String imageLink, int countUser, String name) {
 		super();
 		this.nameChallenge = nameChallenge;
 		this.imageLink = imageLink;
 		this.countUser = countUser;
 		this.name = name;
-		this.tagName1 = tagName1;
-		this.tagName2 = tagName2;
 	}
 	public String getNameChallenge() {
 		return nameChallenge;
@@ -41,16 +36,5 @@ public class ChallengeDataList {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getTagName1() {
-		return tagName1;
-	}
-	public void setTagName1(String tagName1) {
-		this.tagName1 = tagName1;
-	}
-	public String getTagName2() {
-		return tagName2;
-	}
-	public void setTagName2(String tagName2) {
-		this.tagName2 = tagName2;
-	}
+	
 }
